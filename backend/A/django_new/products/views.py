cbf1fb4d376c9df9ab3ab72d978fdbfecf018ce8
ry_moriyama <moriforest537@gmail.com>
2023-06-25 18:51:18 +0900

    wip

--- backend/A/django_new/products/views.py
+++ backend/A/django_new/products/views.py
@@ -4,9 +4,26 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .models import Product
 from .serializers import ProductSerializer
-
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 class ProductViewSet(viewsets.ModelViewSet):
 
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+@api_view(['POST'])
+def product_list(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    message = "succsess"
+    return Response({'message': message}, status=status.HTTP_200_OK)
+
+@api_view(['GET'])
+def get_product_list(request):
+
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
